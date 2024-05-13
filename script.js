73bae6896099d6091ad371bd8a4a5d6f01d5b12f
karloskarb <127998394+karloskarb@users.noreply.github.com>
2024-05-13 11:50:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 document.querySelector('.hamburger').addEventListener('click', function() {
     const sidebar = document.querySelector('aside');
     if (sidebar.style.left === '0px') {
-        sidebar.style.left = '-200px';
-        document.querySelector('.content-area').style.marginLeft = '20px';
+        sidebar.style.left = '-200px'; // Hide sidebar
+        document.querySelector('.content-area').style.marginLeft = '20px'; // Reduce margin
     } else {
-        sidebar.style.left = '0px';
-        document.querySelector('.content-area').style.marginLeft = '220px';
+        sidebar.style.left = '0px'; // Show sidebar
+        document.querySelector('.content-area').style.marginLeft = '220px'; // Increase margin
     }
 });
